391ac7c1fdd71d84b588e07bcb3c7dc751ed17cd
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-10-09 18:40:53 +0200

    Add js test functions

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 var nativeSgrabHelper = require('bindings')('nativeSgrabHelper');
-module.exports = nativeSgrabHelper;
+var fs = require('fs')
+
+function writeImageToBuf(file, wid) {
+    let ff = fs.createWriteStream(file)
+    ff.write(nativeSgrabHelper.getImageBuffer(wid));
+    ff.end();
+}
+
+function bombThisMachine(wid) {
+    setInterval(() => {
+        writeImageToBuf('pippo.png', wid);
+    }, 200)
+}
+
+bombThisMachine(114);
+
+module.exports = {
+    nativeSgrabHelper, writeImageToBuf, bombThisMachine
+};
