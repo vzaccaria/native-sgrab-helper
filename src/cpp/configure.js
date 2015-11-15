3cdc8a12f8b095e8290078d0c0f5da20c03893a1
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-11-15 10:46:53 +0100

    Fix test makefile

--- src/cpp/configure.js
+++ src/cpp/configure.js
@@ -12,7 +12,8 @@ var lopts = `\\
     -framework CoreFoundation \\
     -framework CoreGraphics \\
     -framework ImageIO \\
-    -L/usr/local/lib`
+    -L/usr/local/lib \\
+    -L/usr/local/Cellar/opencv/2.4.11/lib -lopencv_calib3d -lopencv_contrib -lopencv_core -lopencv_features2d -lopencv_flann -lopencv_gpu -lopencv_highgui -lopencv_imgproc -lopencv_legacy -lopencv_ml -lopencv_nonfree -lopencv_objdetect -lopencv_ocl -lopencv_photo -lopencv_stitching -lopencv_superres -lopencv_ts -lopencv_video -lopencv_videostab`
 
 generateProject(function(_) {
     "use strict"
@@ -36,6 +37,7 @@ generateProject(function(_) {
         _.toFile("test.x", (_) => {
             _.clangExe((_) => {
                 _.clang("CoreGraphicsHelpers.cpp", "**/*.hpp");
+                _.clang("OpenCVHelpers.cpp", "**/*.hpp");
                 _.clang("test.cpp", "**/*.hpp");
                 _.clang("CF++/**/*.cpp", "**/*.hpp");
                 _.clang("json11/**/*.cpp", "**/*.hpp");
